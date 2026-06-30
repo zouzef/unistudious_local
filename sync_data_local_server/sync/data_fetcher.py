55987ec88df7ea48ab4ffcc87757c29373ed5b41
Youssef <youssefkasmi05@gmail.com>
2026-06-30 13:36:07 +0100

    testing script

--- sync_data_local_server/sync/data_fetcher.py
+++ sync_data_local_server/sync/data_fetcher.py
@@ -11,7 +11,7 @@ import os
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from core.api_client import APIClient, TokenExpiredError
-
+# youssef test
 
 class DataFetcher:
     """Fetches data from remote server with date filtering"""
